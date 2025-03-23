6c0f874d8da565bf85e2ba3dc2959d8e97b4056c
OBEIIDAT <107028119+OBEIIDAT@users.noreply.github.com>
2025-03-23 23:38:07 +0300

    Delete assets directory

--- assets/js/login-script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    const footer = document.createElement("div");
-    footer.className = "yoopixel-footer-links";
-    footer.innerHTML = '<a href="https://www.yoopixel.com" target="_blank">YOOPixel</a> | <a href="https://portal.yoopixel.com" target="_blank">YOOPixel Portal</a>';
-    document.body.appendChild(footer);
-});
